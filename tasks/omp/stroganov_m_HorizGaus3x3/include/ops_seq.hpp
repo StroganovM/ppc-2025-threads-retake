d80dfdc3e19d4a636e6f3795e8cec4749a9b44c4
Mikhail Andreevich <mikhail_stroganov@bk.ru>
2025-09-11 15:21:40 +0300

    add task

---
+++ tasks/omp/stroganov_m_HorizGaus3x3/include/ops_seq.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace stroganov_m_horiz_gaus3x3_omp {
+
+class ImageFilterOmp : public ppc::core::Task {
+ public:
+  explicit ImageFilterOmp(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+ private:
+  std::vector<double> input_;
+  std::vector<double> output_;
+  int width_;
+  int height_;
+  int kernel_size_ = 3;
+  std::vector<int> kernel_;
+};
+
+}  // namespace stroganov_m_horiz_gaus3x3_omp
