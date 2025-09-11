5d8efeadaf12bba72fc4a58641369d680bef6611
Mikhail Andreevich <mikhail_stroganov@bk.ru>
2025-09-11 18:57:16 +0300

    fix perftest

--- tasks/omp/stroganov_m_HorizGaus3x3/perf_tests/main.cpp
+++ tasks/omp/stroganov_m_HorizGaus3x3/perf_tests/main.cpp
@@ -11,8 +11,8 @@
 #include "omp/stroganov_m_HorizGaus3x3/include/ops_omp.hpp"
 
 TEST(stroganov_m_HorizGaus3x3_omp, test_pipeline_run) {
-  constexpr size_t kWidth = 15000;
-  constexpr size_t kHeight = 15000;
+  constexpr size_t kWidth = 17000;
+  constexpr size_t kHeight = 17000;
   std::vector<double> input_image(kWidth * kHeight, 0.0);
   std::vector<double> output_image(kWidth * kHeight, 0.0);
   std::vector<double> expected(kWidth * kHeight, 0.0);
@@ -65,8 +65,8 @@ TEST(stroganov_m_HorizGaus3x3_omp, test_pipeline_run) {
 }
 
 TEST(stroganov_m_HorizGaus3x3_omp, test_task_run) {
-  constexpr size_t kWidth = 15000;
-  constexpr size_t kHeight = 15000;
+  constexpr size_t kWidth = 17000;
+  constexpr size_t kHeight = 17000;
   std::vector<double> input_image(kWidth * kHeight, 0.0);
   std::vector<double> output_image(kWidth * kHeight, 0.0);
   std::vector<double> expected(kWidth * kHeight, 0.0);
