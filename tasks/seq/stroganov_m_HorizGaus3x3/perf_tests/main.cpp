25bd44840c1bd08ef1fec13f4e4504939107f0c4
Mikhail Andreevich <mikhail_stroganov@bk.ru>
2025-09-14 17:20:23 +0300

    15000

--- tasks/seq/stroganov_m_HorizGaus3x3/perf_tests/main.cpp
+++ tasks/seq/stroganov_m_HorizGaus3x3/perf_tests/main.cpp
@@ -11,8 +11,8 @@
 #include "seq/stroganov_m_HorizGaus3x3/include/ops_seq.hpp"
 
 TEST(stroganov_m_HorizGaus3x3_seq, test_pipeline_run) {
-  constexpr size_t kWidth = 17000;
-  constexpr size_t kHeight = 17000;
+  constexpr size_t kWidth = 15000;
+  constexpr size_t kHeight = 15000;
   std::vector<double> input_image(kWidth * kHeight, 0.0);
   std::vector<double> output_image(kWidth * kHeight, 0.0);
   std::vector<double> expected(kWidth * kHeight, 0.0);
@@ -78,8 +78,8 @@ TEST(stroganov_m_HorizGaus3x3_seq, test_pipeline_run) {
 }
 
 TEST(stroganov_m_HorizGaus3x3_seq, test_task_run) {
-  constexpr size_t kWidth = 17000;
-  constexpr size_t kHeight = 17000;
+  constexpr size_t kWidth = 15000;
+  constexpr size_t kHeight = 15000;
   std::vector<double> input_image(kWidth * kHeight, 0.0);
   std::vector<double> output_image(kWidth * kHeight, 0.0);
   std::vector<double> expected(kWidth * kHeight, 0.0);
